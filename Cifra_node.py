fcda5fef98d6cd83f14f9830ff56f12e84f7f247
Erica Feitosa <erika.feitosa2004@gmail.com>
2026-08-30 20:06:08 -0300

    Adicionando decifragem em Cifra_node.py e menu simples de opções em main.py

--- Cifra_node.py
+++ Cifra_node.py
@@ -1,13 +1,21 @@
+import unicodedata
+
 class Cifra_de_Vigenere():
     def __init__(self, chave):
         self.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        self.key = chave.upper()
-
+        self.key = self.remover_acentos(chave.upper())
         self.letter_to_index = dict(zip(self.alphabet, range(len(self.alphabet))))
         self.index_to_letter = dict(zip(range(len(self.alphabet)), self.alphabet))
 
+    @staticmethod
+    def remover_acentos(texto):
+        #Normaliza o texto de composição de caracteres Unicode, removendo acentos e diacríticos.
+        texto_nfd = unicodedata.normalize('NFD', texto)
+        texto_sem_acento = ''.join(c for c in texto_nfd if unicodedata.category(c) != 'Mn')
+        return texto_sem_acento
+
     def cifragem(self, message):
-        message = message.upper()
+        message = self.remover_acentos(message.upper())
         #message = message.split()
         mesage_cifrada = ''
         j = 0
@@ -36,3 +44,19 @@ class Cifra_de_Vigenere():
 
         return mesage_cifrada
 
+    def decifragem(self, mesage_cifrada):
+        mesage_cifrada = mesage_cifrada.upper()
+        mensagem_original = ''
+        j = 0
+
+        for letter in mesage_cifrada:
+            if letter not in self.alphabet:
+                mensagem_original += letter
+                continue
+            #Lógica matemática inversa da cifragem, subtraindo o índice da letra da chave do índice da letra cifrada.
+            number = (self.letter_to_index[letter] - self.letter_to_index[self.key[j]]) % len(self.alphabet)
+            mensagem_original += self.index_to_letter[number]
+
+            j = (j + 1) % len(self.key)
+
+        return mensagem_original
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import Cifra_node
 
 def main():
+    opcao = input("Escolha uma opção:\n1. Cifrar mensagem\n2. Decifrar mensagem\nDigite o número da opção desejada: ")
     chave = input("Digite a chave: ")
     mensagem = input("Digite a mensagem: ")
 
     cifra = Cifra_node.Cifra_de_Vigenere(chave)
-    mensagem_cifrada = cifra.cifragem(mensagem)
-
-    print("Mensagem cifrada:", mensagem_cifrada)
-
+    if opcao == "1":
+        print("Mensagem cifrada:", cifra.cifragem(mensagem))
+    elif opcao == "2":
+        print("Mensagem original:", cifra.decifragem(mensagem))
+    else:
+        print("Opção inválida.")
 
 if __name__ == "__main__":
     main()
